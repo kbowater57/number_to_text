74aae591623a8fa8649e327f21a7abe25fbe63f6
kbowater57 <49311506+kbowater57@users.noreply.github.com>
2019-05-06 12:47:19 +0100

    Started project. Can now print single digit numbers
    Added all files other than .cpp file to .gitignore

---
+++ number_to_text/number_to_text/number_to_text.cpp
@@ -0,0 +1,29 @@
+// number_to_text.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+std::string units[10] = { "zero", "one","two","three","four", "five","six","seven","eight","nine" };
+std::string excuse = "I don't know how to print that yet";
+int main()
+{
+    std::cout << "Type your number\n"; 
+	int digits;
+	std::cin >> digits;
+		if (digits < 10) {
+			std::cout << units[digits];
+	}
+		else {
+			std::cout << excuse;
+		}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
